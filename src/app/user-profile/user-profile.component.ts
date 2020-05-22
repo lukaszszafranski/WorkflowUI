0c002d0ee92ad758a54539eba89c90cc504b425e
lukaszszafranski <lukasz.szafranski16@wp.pl>
2020-05-22 21:07:33 +0200

    user profile validation fixed.

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -40,7 +40,7 @@ export class UserProfileComponent implements OnInit {
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       username: ['', Validators.required],
-      password: ['', Validators.required]
+      password: ['', Validators.required, Validators.minLength(6)]
     });
   }
 
