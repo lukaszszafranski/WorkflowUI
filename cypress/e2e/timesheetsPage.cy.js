d1e881564c6119621fadc89ab89ce5455454b9e8
Milosz Sobiecki <sobiecki.mj@gmail.com>
2023-01-20 00:40:15 +0100

    added timesheets tests

---
+++ cypress/e2e/timesheetsPage.cy.js
@@ -0,0 +1,23 @@
+import { LoginPage } from "../support/Pages/logInPage";
+import { Redirects } from "../support/Pages/redirects";
+
+describe('Timesheets Page', () => {
+
+    beforeEach(() => {
+        cy.visit('https://portale-korporacyjne.azurewebsites.net/login')
+        
+        const loginPage = new LoginPage();
+        const redirects = new Redirects();
+
+        var username = 'ŁUKASZ.TOPAJAC';
+        var password = '123456';
+    
+        loginPage.logInUsingCredentials(username, password);
+        redirects.redirectToTimesheetsPage();
+    })
+
+it.only('As a user I want to open first timesheet from the top', () => {
+    cy.get('.card-header').contains('Styczeń 2023').click();
+    cy.get('.page-header-title').should('contain.text', 'Styczeń 2023');
+});
+})
